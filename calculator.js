0dd04d065d314fccb988e1304c3c1ea6b18c8d6f
[200~155949712+Katusyo@users.noreply.github.com~ <155949712+Katusyo@users.noreply.github.com>
2025-10-25 09:37:08 +0900

    Progress of calculator project
    + included the divide function

--- calculator.js
+++ calculator.js
@@ -18,13 +18,22 @@ function subtract(a, b) {
 subtract(5, 3);
 
 function multiply(a, b) {
-    console.log("input a:", a);
+    console.log("input a:", parseFloat(a));
     console.log("input b:", b);
     let sum = a * b;
     console.log("Multiplied sum", sum)
 };
 
 multiply(5, 3);
+
+function divide(a, b) {
+    console.log("input a:", parseFloat(a));
+    console.log("input b:", parseFloat(b));
+    let sum = a / b;
+    console.log("Divided sum", sum)
+};
+
+divide(5, 3);
 
 const sum = function(array) {
   return array.reduce((total, current) => total + current, 0);
